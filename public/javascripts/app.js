da84415126a99a1020d52dd119d05c845c2979f0
Colin Orr <colin.orr@gmail.com>
2013-09-10 15:55:24 -0400

    Implemented active/inactive panes to help with the transitions

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,21 @@
 var app = Sammy();
 
-app.get('#:pane', function(){
-  $('.selected.pane').removeClass('selected');
-  $('#'+ this.params.pane).addClass('selected');
+app.get('#:pane', function(context) {
+  var current = $('.active.pane');
+  var target = $('#' + context.params.pane);
+  if(current[0] === target[0]) return;
+  
+  target.removeClass('inactive');
+  
+  //  Yuck!
+  setTimeout(function() {
+    target.addClass('active');
+    current.removeClass('active');
+  }, 0);
+  
+  setTimeout(function(){
+    current.addClass('inactive');
+  }, 250);
 });
 
 app.run('#login');
