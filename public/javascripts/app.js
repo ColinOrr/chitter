2d79523788c054e07d5bd893393a98ff92cee7eb
Colin Orr <colin.orr@gmail.com>
2013-09-11 17:04:32 -0400

    Added RequireJS and the login view model

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,21 +1,14 @@
-var app = Sammy();
-
-app.get('#:pane', function(context) {
-  var current = $('.active.pane');
-  var target = $('#' + context.params.pane);
-  if(current[0] === target[0]) return;
-  
-  target.removeClass('inactive');
-  
-  //  Yuck!
-  setTimeout(function() {
-    target.addClass('active');
-    current.removeClass('active');
-  }, 0);
-  
-  setTimeout(function(){
-    current.addClass('inactive');
-  }, 250);
+require.config({
+  paths: {
+    jquery:   '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min',
+    knockout: '//cdnjs.cloudflare.com/ajax/libs/knockout/2.3.0/knockout-min',
+    sammy:    '//cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.4/sammy.min'
+  },
+  shim: {
+    sammy: { deps: ['jquery'], exports: 'Sammy' }
+  }
 });
 
-app.run('#login');
+require(['shellViewModel'], function(shellViewModel) {
+  new shellViewModel();
+});
